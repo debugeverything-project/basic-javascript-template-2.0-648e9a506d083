d66ac25c925ce876f9f9bda44c50f1f09b04ac5f
debugeverything-project <95109898+debugeverything-project@users.noreply.github.com>
2023-06-26 10:17:14 -0300

    Update to create PR

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS sim</h1>`;
+appDiv.innerHTML = `<h1>Updated branch</h1>`;
