169858ed38323bf97d812364e6f24dcd8059a140
debugeverything-project <95109898+debugeverything-project@users.noreply.github.com>
2023-07-27 08:27:51 +1000

    fix

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Debung oi kjkjkasdasd hrthrth23452345234 test Everything!?!?!?!</h1>`;
+appDiv.innerHTML = `<h1>Debung oi tste</h1>`;
